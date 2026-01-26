9927445c1cf8670175e8ec2e7cdb07c118d3f72d
Abhee0 <abhishektakale12@gmail.com>
2026-01-26 19:57:35 +0530

    update a call icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,18 @@ export default {
         body: ['Inter', 'sans-serif'],
         ui: ['Open Sans', 'sans-serif'],
       },
+      keyframes: {
+        vibrate: {
+          '0%, 100%': { transform: 'translate(0)' },
+          '20%': { transform: 'translate(-2px, 2px)' },
+          '40%': { transform: 'translate(-2px, -2px)' },
+          '60%': { transform: 'translate(2px, 2px)' },
+          '80%': { transform: 'translate(2px, -2px)' },
+        },
+      },
+      animation: {
+        vibrate: 'vibrate 0.3s linear infinite',
+      },
     },
   },
   plugins: [],
